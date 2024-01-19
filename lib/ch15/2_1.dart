7d71cae20dab45995f29dd157bf2639ca3b8dc37
Ballbot <5252bb@daum.net>
2024-01-19 10:54:22 +0900

    0118

--- lib/ch15/2_1.dart
+++ lib/ch15/2_1.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -21,14 +21,17 @@ class MyAppState extends State<MyApp> {
     http.Response res = await http.get(
         Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
         headers: headers);
+
+    setState(() {
+      result = res.body;
+    });
   }
 
   onPressPost() async {
     try {
       http.Response res = await http.post(
-        Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
-        body: {'title': 'hello', 'body': 'world', 'userId': 1}
-      );
+          Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
+          body: {'title': 'hello', 'body': 'world', 'userId': '1'});
       print('POST RESULT: ${res.statusCode}');
 
       if (res.statusCode == 200 || res.statusCode == 201) {
@@ -53,8 +56,7 @@ class MyAppState extends State<MyApp> {
     try {
       http.Response res = await client.post(
           Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
-          body: {'title': 'hello', 'body': 'world', 'userId': 1}
-      );
+          body: {'title': 'hello', 'body': 'world', 'userId': '1'});
       if (res.statusCode == 200 || res.statusCode == 201) {
         res = await http.get(
             Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
@@ -72,19 +74,14 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Http')),
-        body: Center(
-          child: Column(
-            children: [
+        home: Scaffold(
+            appBar: AppBar(title: Text('Http')),
+            body: Center(
+                child: Column(children: [
               Text('RESULT: $result'),
               ElevatedButton(onPressed: onPressGet, child: Text('Get')),
               ElevatedButton(onPressed: onPressPost, child: Text('Post')),
               ElevatedButton(onPressed: onPressClient, child: Text('Client')),
-            ]
-          )
-        )
-      )
-    )
+            ]))));
   }
 }
